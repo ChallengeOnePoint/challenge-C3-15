188e3669283f429266061ec57ee34936a7ecd0a8
Antony Canut <antony.canut@studentpartner.com>
2016-01-23 16:50:25 +0100

    recuperation de force

--- server.js
+++ server.js
@@ -6,26 +6,12 @@ var _ = require('lodash')
 var http = require('http')
 var express = require('express')
 var bodyParser = require('body-parser')
-var socket = require('socket.io')
 
 var app = express()
-var server = http.createServer(app)
-var io = socket(server)
-
-var posts = []
-var clients = []
-
 app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.json())
 
+var server = http.createServer(app)
 server.listen(port)
-
-io.on('connection', function (socket) {
-  console.log('connected')
-  socket.emit('news', { hello: 'world' })
-  socket.on('my other event', function (data) {
-    console.log(data)
-  })
-})
 
 console.log('Hello world!')
